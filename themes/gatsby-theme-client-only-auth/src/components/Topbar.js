0dbcb5bf60581c629c5cb258f24efeff98909fd7
René Monroy <rene.monroy@gmail.com>
2019-11-12 14:56:26 -0600

    Add Topbar component

--- themes/gatsby-theme-client-only-auth/src/components/Topbar.js
+++ themes/gatsby-theme-client-only-auth/src/components/Topbar.js
@@ -1,33 +1,28 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
+import { Menu as MenuIcon } from "@material-ui/icons";
 
-// import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-// import Drawer from "@material-ui/core/Drawer";
-import IconButton from "@material-ui/core/IconButton";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import MenuIcon from "@material-ui/icons/Menu";
-
-// const useStyles = makeStyles(theme => ({
-//   toolbar: {
-//     flexGrow: 1
-//   },
-//   menuButton: {
-//     marginRight: theme.spacing(2)
-//   },
-//   title: {
-//     flexGrow: 1
-//   }
-// }));
+const useStyles = makeStyles(theme => ({
+  menuButton: {
+    marginRight: theme.spacing(1),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function Topbar() {
+  const classes = useStyles();
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start">
+        <IconButton className={classes.menuButton} color="inherit" edge="start">
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6">Home</Typography>
+        <Typography className={classes.title} variant="h6">
+          Home
+        </Typography>
       </Toolbar>
     </AppBar>
   );
